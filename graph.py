a8d37a45310870ae1e6cd71f02190c64fb334b21
Ayse Rumeysa Ergin <ayserumeysaergin@gmail.com>
2019-02-24 09:45:53 -0600

    python

---
+++ graph.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 24 09:16:08 2019
+
+@author: AyseErgin
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+test = pd.read_csv('test.csv')
+
+x = test[test['ID_code'] == inputID]
+var= 
+plt.plot(var)
+plt.ylabel('some numbers')
+plt.show()
